107f0c294d04195c5addb9a222e54adf5d7fd557
Robert Burek <robertburek@wp.pl>
2019-08-16 19:24:44 +0200

    Widoki  VIEW

---
+++ Widoki  VIEW.sql
@@ -0,0 +1,20 @@
+USE world;
+
+DROP VIEW IF EXISTS PozycjaPKB;
+
+CREATE VIEW PozycjaPKB AS
+SELECT 
+    GNP AS PKB,
+    Name AS Kraj,
+    Continent AS Kontynent,
+    Population AS Ludność,
+    SurfaceArea AS Powierzchnia,
+    LifeExpectancy AS DługośćŻycia
+FROM
+    country
+ORDER BY PKB DESC;
+
+SELECT * FROM world.pozycjapkb;
+SET @nr := 0;
+SELECT @nr := @nr + 1 AS id, pkb, kraj, długośćżycia FROM world.pozycjapkb;
+
